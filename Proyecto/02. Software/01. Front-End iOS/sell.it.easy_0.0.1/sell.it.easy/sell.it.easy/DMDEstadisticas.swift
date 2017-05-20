b5373715e7e922bf7ce9563c76db84e9770e341d
maarkeez <d.marquez.delgado.91@gmail.com>
2017-05-20 13:11:41 +0200

    Instalado pod 'Charts'

---
+++ Proyecto/02. Software/01. Front-End iOS/sell.it.easy_0.0.1/sell.it.easy/sell.it.easy/DMDEstadisticas.swift
@@ -0,0 +1,22 @@
+//
+//  DMDEstadisticas.swift
+//  sell.it.easy
+//
+//  Created by David Márquez Delgado on 20/5/17.
+//  Copyright © 2017 David Márquez Delgado. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+
+class DMDEstadisticas: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+  
+
+}
