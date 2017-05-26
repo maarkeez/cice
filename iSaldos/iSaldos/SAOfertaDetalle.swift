43a12f1eb6272ca28cda08c4224cbb1534bf60c1
cice <cice@aula8PovMacN05.local>
2017-05-26 20:00:17 +0200

    Desarrollo de vista detalle para ofertas

--- iSaldos/iSaldos/SAOfertaDetalle.swift
+++ iSaldos/iSaldos/SAOfertaDetalle.swift
@@ -47,8 +47,33 @@ class SAOfertaDetalle: UITableViewController {
     //MARK: - IBOutltes
     @IBOutlet weak var myImagen: UIImageView!
     @IBOutlet weak var myMapa: MKMapView!
+    @IBOutlet weak var myNombre: UILabel!
+    @IBOutlet weak var myFecha: UILabel!
+    @IBOutlet weak var myMasInformacion: UILabel!
+    @IBOutlet weak var myDescripcionAsociado: UILabel!
+    @IBOutlet weak var myDireccionAsociado: UILabel!
+    @IBOutlet weak var myTelefonoMovil: UILabel!
+    @IBOutlet weak var myEmail: UILabel!
+    @IBOutlet weak var myMapAsociado: MKMapView!
+    @IBOutlet weak var myTelefonoFijo: UIButton!
+    @IBOutlet weak var myWebURL: UIButton!
     
     //MARK: - IBActions
+    
+    @IBAction func myLlamarFijoACTION(_ sender: UIButton) {
+        //Recuperar el teléfono
+        let telefono = sender.titleLabel?.text
+        if let telefonoDes = telefono {
+            llamar(telefonoDes)
+        }
+        
+    }
+    
+    @IBAction func myCargarPaginaACTION(_ sender: UIButton) {
+        if let web = sender.titleLabel?.text {
+            accederURL(web)
+        }
+    }
     
     
     //MARK: - LIFE VC
@@ -58,11 +83,48 @@ class SAOfertaDetalle: UITableViewController {
         if let imagen = detalleImagen {
             myImagen.image = imagen
         }
+        if let oferta = oferta {
+            myNombre.text = oferta.nombre ?? ""
+            myFecha.text = oferta.fechaFin ?? ""
+            myMasInformacion.text = oferta.masInformacion ?? ""
+            myDescripcionAsociado.text = oferta.asociado?.descripcion ?? ""
+            myDireccionAsociado.text = oferta.asociado?.direccion ?? ""
+            myTelefonoMovil.text = oferta.asociado!.telefonoMovil ?? ""
+            myEmail.text = oferta.asociado!.mail ?? ""
+            myWebURL.titleLabel?.text = oferta.asociado!.web ?? ""
+            myTelefonoFijo.titleLabel?.text = oferta.asociado!.telefonoFijo! ?? ""
+        }
     }
 
    
 
     //MARK: - Utils
-   
+    ///Permite llamar a un número de teléfono
+    func llamar(_ telefono : String){
+        if let phoneCallURL = URL(string: "tel://\(telefono)") {
+            let application:UIApplication = UIApplication.shared
+            if (application.canOpenURL(phoneCallURL)) {
+                application.open(phoneCallURL, options: [:], completionHandler: nil)
+            }
+        }
+    }
+    
+    ///Permite llamar a un número de teléfono
+    func accederURL(_ web : String){
+        if let webURL = URL(string: "\(web)") {
+            let application:UIApplication = UIApplication.shared
+            if (application.canOpenURL(webURL)) {
+                application.open(webURL, options: [:], completionHandler: nil)
+            }
+        }
+    }
+
+    ///Mostrar un viewcontroller con una web
+    func muestraPaginaWebAsociado(_ url: String){
+        let webVC = self.storyboard?.instantiateViewController(withIdentifier: "SAWebViewController") as! SAWebViewController
+        
+        webVC.urlWeb = stringWeb
+        present(webVC, animated: true, completion: nil)
+    }
 
 }
---
+++ iSaldos/iSaldos/SAWebViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SAWebViewController.swift
+//  iSaldos
+//
+//  Created by cice on 26/5/17.
+//  Copyright © 2017 icologic. All rights reserved.
+//
+
+import UIKit
+
+class SAWebViewController: UIViewController {
+
+    //MARK: - Variables Locales
+    var urlWeb : String?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
